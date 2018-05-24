e3fe8e23f00663a97cc8e9cbc26df8bb8aeaf19e
linusmcm <linus.mcmanamey@gmail.com>
2018-05-24 11:31:57 +1000

    test

--- src/mapper.scala
+++ src/mapper.scala
@@ -1,16 +1,7 @@
-
- //val src = io.Source.fromFile("C:/Users/linus/Documents/scala/datasets/temp.csv")
- //val src = io.Source.fromFile("C:\\Users\\linus\\IdeaProjects\\Activity1\\src\\first.txt")
-  //for(lines <- src.getLines)
- for (lines <- scala.io.Source.stdin.getLines)
+for (lines <- scala.io.Source.stdin.getLines)
  {
     var linesLower = lines.toLowerCase()
     linesLower = linesLower.replaceAll("""[\p{Punct}]""", "")
     linesLower.split(" ").foreach(x => println(s"$x\t1"))
 
   }
-
-
-
-
- //C:\Users\linus\IdeaProjects\Activity1\src\first.txt
