98c7da3eeaa2795d883f0f61a6c671ba466b34f1
linusmcm <linus.mcmanamey@gmail.com>
2018-05-24 11:51:27 +1000

    test

--- src/mapper.scala
+++ src/mapper.scala
@@ -3,5 +3,4 @@ for (lines <- scala.io.Source.stdin.getLines)
     var linesLower = lines.toLowerCase()
     linesLower = linesLower.replaceAll("""[\p{Punct}]""", "")
     linesLower.split(" ").foreach(x => println(s"$x\t1"))
-
   }
